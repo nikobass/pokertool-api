ff99309ebdce4217906cfb8c2fb6c0d605f10991
PascalVALLADE <shoshoie@free.fr>
2021-06-12 18:59:28 +0200

    feed router.js

--- app/router.js
+++ app/router.js
@@ -1,22 +1,50 @@
 const express = require('express');
 const path = require('path');
 
-// IMPORT DES CONTROLLERS
+// CONTROLLERS' IMPORT 
 const mainController = require('./controllers/mainController');
 
 const router = express.Router();
 
-// LES ROUTES et leur actions
-// exemple : 
-//router.route('/lists/:id')
-//    .get(listController.getOneList)
-//    .patch(listController.updateList)
-//    .delete(listController.deleteList);
+// AUTHENTIFICATION + CONNEXION
+// create user
+router.post('/signup', authController.createUser);
+// authentification
+router.post('/signin', authController.authUser);
+// reset password
+router.get('/resetPassword/:token', authController.resetPassword);
 
-router.get('/', (_, response) => {
-    console.log("COUCOU !");
-});
+
+// PROFIL
+// user's profil
+router.get('/profil/:userId', profilController.getProfil);
+// delete
+router.delete('/profil/:userId', profilController.deleteProfil);
+// update
+router.patch('/profil/:userId', profilController.updateProfil);
+
+// TOURNAMENT
+// tournaments' datas
+router.get('/tournaments', tournamentController.getAllTournaments);
+// create a tournament
+router.post('/tournament', tournamentController.createTournament);
+// tournament's datas
+router.get('/tournament/:id', tournamentController.getOneTournament);
+// update a tournament
+router.patch('/tournament/:id', tournamentController.updateTournament);
+// delete a tournament
+router.delete('/tournament/:id', tournamentController.deleteTournament);
+// tournament's user
+router.get('timer/:tournamentId', tournamentController.getTimerTournament);
+
+// CHIP
+// user's chips
+router.get('/chip/:userId', chipController.getChip);
+// create user's chips
+router.post('/chip/:userId', chipController.createChip);
+// update user's chips
+router.patch('/chip/:userId', chipController.updateChip);
 
 router.use(mainController.error404);
 
-module.exports = router;
+module.exports = router; 
