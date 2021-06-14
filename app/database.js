6e15f7ad2736a0b2e99e61265460dee78c3c9277
PascalVALLADE <shoshoie@free.fr>
2021-06-14 08:50:36 +0200

    database.js : modify 'dates in snake_case'

--- app/database.js
+++ app/database.js
@@ -5,8 +5,8 @@ const sequelize = new Sequelize(process.env.PG_URL, {
         // On dit à sequelize qu'on utilise la version snakeCase plutôt que camelCase
         underscored: true,
         // on modifie le nom des colonnes de date, qui par défaut sont en camelCase
-        //createdAt: 'created_at',
-        //updatedAt: 'updated_at',
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
         //évite de tester si colonne « créé le » dans les tables existe
         timestamps:false,
     }
