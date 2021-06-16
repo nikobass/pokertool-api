45447e422b86fe73ba59ff9b520ab17a5e1466e8
PascalVALLADE <shoshoie@free.fr>
2021-06-16 19:49:10 +0200

    profil OK

--- app/controllers/profilController.js
+++ app/controllers/profilController.js
@@ -2,7 +2,8 @@ const emailValidator = require("email-validator");
 const bcrypt = require("bcrypt");
 const { User } = require("../models");
 const sanitizeHtml = require('sanitize-html');
-const { BcryptData } = require('../utils')
+const { BcryptData } = require('../utils');
+const { Op } = require('sequelize');
 //const { formatEmailValid } = require("../middleware/email");
 
 const profilController = {
@@ -16,13 +17,13 @@ const profilController = {
    //le pseudo existe déjà
    const userPseudo = await User.findOne({
     where: {
-     user_name: req.body.nickname,
+     user_name: req.body.user_name,
     },
    });
    if (userPseudo) {
     return res.json({ error: "Ce pseudo existe déjà." });
    }
-   
+
     //l'email existe déjà
     const userEmail = await User.findOne({
       where: {
@@ -32,14 +33,14 @@ const profilController = {
     if (userEmail) {
         return res.json({ error : "mail a déjà été utilisé sur notre site."})
     }
-    
+  
     //format d'email invalide
     if (!emailValidator.validate(email)) {
         return res.json ({error : "Votre email doit correspondre au format suivant monMail99@gmail.com."})
         }
 
     // Le mot de passe doit contenir au moins 8 caractères et doit contenir au moins une majuscule et un chiffre.
-    const verifUserEmail = email.split("@")[0];
+    const verifUserPwd = req.body.password;
     let isNumeric = false;
     let isUpperCase = false;
     let nbLetters = false;
@@ -47,13 +48,13 @@ const profilController = {
     let i = 0;
 
         // On vérifie la longueur du mail
-    if (verifUserEmail.length >= 8) {
+    if (verifUserPwd.length >= 8) {
         nbLetters = true;
     }
 
     // On vérifie la présence d'une majuscule et d'un chiffre
-    while (i < verifUserEmail.length) {
-        character = verifUserEmail.charAt(i);
+    while (i < verifUserPwd.length) {
+        character = verifUserPwd.charAt(i);
 
         // est-ce un nombre ?
         if (!isNaN(character * 1)) {
@@ -68,12 +69,9 @@ const profilController = {
     }
 
     if (!isNumeric || !isUpperCase || !nbLetters) {
-        return res.json ({ error : "Votre email doit contenir une lettre majuscule et un chiffre. Il doit également comporté au minimum 8 caractères."})
+        return res.json ({ error : "Votre mot de passe doit contenir une lettre majuscule et un chiffre. Il doit également comporté au minimum 8 caractères."})
     }
-    
-  //  const isFormatMailValid = formatEmailValid (email, res)
-  //  console.log(isFormatMailValid)
-  
+ 
      // l'e-mail et la confirmation ne correspondent pas
    if (req.body.email !== req.body.emailConfirm) {
     return res.json({ error: "La confirmation de l'email ne correspond pas." });
@@ -139,7 +137,6 @@ const profilController = {
       res.status(500).json({ error: `l'utilisateur ${userId} n'a pas été trouvé !` });
     }
   } catch (err) {
-    console.trace(err);
     res.status(500).send(err);
   }
 },
@@ -148,8 +145,7 @@ const profilController = {
   updateProfil: async (req, res, next) => {
   try {
     const data = req.body;
-    const errors = [];
-    console.log(data)
+
     // on vérifie l'id de la route  
     const id = parseInt(req.params.userId, 10);
       if (isNaN(id)) {
@@ -163,18 +159,20 @@ const profilController = {
       }
 
       // On vérifie le pseudo renseigné
-      if (typeof data.nickname !== 'undefined') {
-          if (data.nickname === '') {
+      if (typeof data.user_name !== 'undefined') {
+          if (data.user_name === '') {
               return res.json({error: `le speudo doit être renseigné`});
           }
       }
 
-      //le pseudo existe déjà // A FACTORISER
+      //le pseudo existe déjà
       const userPseudo = await User.findOne({
         where: {
-        user_name: data.nickname,
+          id: {[Op.ne]: id},
+          user_name: data.user_name,
         },
       });
+
       if (userPseudo) {
         return res.json({ error: "Ce pseudo existe déjà." });
       }
@@ -182,8 +180,8 @@ const profilController = {
     //l'email existe déjà HORMIS LE SEIN
     const userEmail = await User.findOne({
       where: {
-      email: data.email,
-      //id: !data.userId,
+        email: data.email,
+        id: {[Op.ne]: id},
       },
     });
     if (userEmail) {
@@ -196,26 +194,26 @@ const profilController = {
         }
 
     // Le mot de passe doit contenir au moins 8 caractères et doit contenir au moins une majuscule et un chiffre.
-    const verifUserEmail = data.email.split("@")[0];
+    const verifUserPwd = data.password;
     let isNumeric = false;
     let isUpperCase = false;
     let nbLetters = false;
     let character = "";
     let i = 0;
 
-    // On vérifie la longueur du mail
-    if (verifUserEmail.length >= 8) {
+    // On vérifie la longueur du pwd
+    if (verifUserPwd.length >= 8) {
         nbLetters = true;
     }
 
     // On vérifie la présence d'une majuscule et d'un chiffre
-    while (i < verifUserEmail.length) {
-        character = verifUserEmail.charAt(i);
+    while (i < verifUserPwd.length) {
+        character = verifUserPwd.charAt(i);
 
         // est-ce un nombre ?
         if (!isNaN(character * 1)) {
             isNumeric = true;
-            // est-ce une majuscule
+        // est-ce une majuscule
         } else {
             if (character === character.toUpperCase()) {
                 isUpperCase = true;
@@ -225,16 +223,18 @@ const profilController = {
     }
 
     if (!isNumeric || !isUpperCase || !nbLetters) {
-      return res.json ({ error : "Votre email doit contenir une lettre majuscule et un chiffre. Il doit également comporté au minimum 8 caractères."})
+      return res.json ({ error : "Votre mot de passe doit contenir une lettre majuscule et un chiffre. Il doit également comporté au minimum 8 caractères."})
     }
 
     //On assaini les valeurs texte
-    data.nickname = sanitizeHtml(data.nickname);
-        
+    data.user_name = sanitizeHtml(data.user_name);
+
+    //On crypte le PWD
     data.password =  BcryptData(data.password);
-      
+
     // update
     const userSaved = await user.update(data);
+
     res.json(userSaved);
 
     } catch (error) {
